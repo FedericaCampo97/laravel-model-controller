58960a4da10515231d742d54080cedc7ce03363b
FedericaCampo97 <federica.campo97@gmail.com>
2023-10-29 20:07:29 +0100

    Exercise complete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,31 @@
 </head>
 
 <body>
-
-    <h1 class="text-danger">Movies</h1>
+    <div class="mt-5">
+        <h1 class="text-danger text-center">Movies</h1>
+    </div>
+    <div class="d-flex flex-wrap p-5 text-center">
+        @foreach($movies as $movie)
+        <div class="col-3">
+            <div class="card m-3">
+                <div class="card-body">
+                    <h4 class="card-title">
+                        {{$movie->title}}
+                    </h4>
+                    <h5 class="card-subtitle mb-2 text-body-secondary">
+                        {{$movie->original_title}}
+                    </h5>
+                    <p class="card-text">
+                        {{$movie->nationality}}
+                    </p>
+                    <h6>
+                        {{$movie->vote}}
+                    </h6>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
 
 
 </body>
